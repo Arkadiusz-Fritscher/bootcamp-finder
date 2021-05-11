1357f20065d22724b5003ad2ad15495ed63d4ff9
Arek <arkadiuszp@arcor.de>
2021-05-11 13:05:38 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+#app {
+  width: 96%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Content</h1>
+    <div>
+      <div v-for="bootcamp in bootcamps" :key="bootcamp.slug">
+        {{ bootcamp.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const bootcamps = await $content('bootcamps').fetch()
+
+    return {
+      bootcamps,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
